5ac92f8dd944b4856e49a21ac564c2fc6077a169
Graham <graham.haphey@gmail.com>
2019-04-22 06:33:07 -0500

    enemy behavior start

--- Assets/Scripts/ZombieController.cs
+++ Assets/Scripts/ZombieController.cs
@@ -3,8 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyController : MonoBehaviour
+public class ZombieController : MonoBehaviour
 {
+    [SerializeField] private float speed = 2;
     private HealthController myHealth;
     // Start is called before the first frame update
     void Start()
@@ -17,12 +18,17 @@ public class EnemyController : MonoBehaviour
     {
         if(myHealth.IsAlive())
         {
-
+            Move();
         }
         else
         {
             Death();
         }
+    }
+
+    private void Move()
+    {
+        transform.position += new Vector3(speed * Time.deltaTime, 0);
     }
 
     private void Death()
